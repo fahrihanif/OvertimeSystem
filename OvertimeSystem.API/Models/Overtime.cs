b89f6f0ef75ab82474164ab3337f7796d5f36b56
Fahri Hanif <fahrihanif03@gmail.com>
2025-11-25 00:10:40 +0700

    Delete Overtime.cs

--- OvertimeSystem.API/Models/Overtime.cs
+++
@@ -1,8 +0,0 @@
-namespace OvertimeSystem.API.Models;
-
-public class Overtime
-{
-    public Guid Id { get; set; }
-    public DateTime Month { get; set; }
-    public int TotalHours { get; set; }
-}
